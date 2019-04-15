a3f024753766baea05af469dcd946dcb0442106a
一瀬将治 <ichinosemasaharu@team-lab.com>
2019-04-15 13:30:38 +0900

    fix:行間調整

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -13,49 +13,85 @@ use App\Http\Controllers\Controller;
 
 class ProductController extends BaseController
 {
+
   public function index(Request $request)
+
   {
+
     $product = Product::all();
+
     return response()->json(['product' => $product]);
+
   }
+
   public function store(Request $request)
+
   {
+
     $request->validate([
+
       'price' => 'integer',
       'title' => 'string|max:100',
       'description' => 'string|max:500',
       'image' => 'integer',
+
     ]);
+
     $product = new Product;
+
     $product->price = $request->price;
+
     $product->title = $request->title;
+
     $product->description = $request->description;
+
     $product->image = $request->image;
+
     $product->save();
+
   }
+
   public function show($id)
   {
+
     $product = Product::find($id);
+
     return response()->json(['product' => $product]);
+
   }
+
   public function update(Request $request, $id)
   {
+
     $request->validate([
+
       'price' => 'integer',
       'title' => 'string|max:100',
       'description' => 'string|max:500',
       'image' => 'integer',
+
     ]);
+
     $product = Product::find($id);
+
     $product->price = $request->price;
+
     $product->title = $request->title;
+
     $product->description = $request->description;
+
     $product->image = $request->image;
+
     $product->save();
+
   }
+
   public function destroy($id)
   {
+
     $product = Product::find($id);
+
     $product->delete();
+
   }
 }
